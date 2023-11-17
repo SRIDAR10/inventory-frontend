e528418af9c1ea99f69dec751fbaec12c9deaf9a
Sridar C S <sridarsaikrishnan@preskale.com>
2023-11-17 23:08:47 +0530

    fix url bug

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen flex-col">
       <h1 className="text-3x1 mb-4">Inventory Management Software</h1>
-      <Link href="/inventory/home">View Dashboard</Link>
+      <Link href="/inventory/home/dashboard">View Dashboard</Link>
     </div>
   );
 }
