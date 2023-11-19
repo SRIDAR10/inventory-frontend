ada97c6cb0951f518f6603546b7e0535ef9f5c5a
Sridar C S <sridarsaikrishnan@preskale.com>
2023-11-19 23:08:44 +0530

    log env variables

--- app/(back-office)/inventory/home/dashboard/page.tsx
+++ app/(back-office)/inventory/home/dashboard/page.tsx
@@ -4,9 +4,10 @@ import React from "react";
 import DashboardActivites from "@/components/inventory/DashboardActivites";
 import ProductDetails from "@/components/inventory/ProductDetails";
 import TopSellingItems from "@/components/inventory/TopSellingItems";
-import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
+  console.log("CLIENT SECRET", process.env.NEXT_PUBLIC_GITHUB_SECRET as string);
+  console.log("CLIENT ID",  process.env.NEXT_PUBLIC_GITHUB_ID as string);
   return (
     <>
       <div className="bg-[#ecf2fe] border-b border-slate-300">
