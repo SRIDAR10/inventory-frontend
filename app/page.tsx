7c25c26ddd94872e8381e3256497d752c9a7f46c
Sridar C S <sridarsaikrishnan@preskale.com>
2023-11-19 22:41:52 +0530

    revert / route changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
-  router.replace("/inventory/home/dashboard");
-  return <></>;
+  return (
+    <div className="flex items-center justify-center min-h-screen flex-col">
+      <h1 className="text-3x1 mb-4">Welcome to StockPulse</h1>
+      <Link href="/inventory/home/dashboard">View Dashboard</Link>
+    </div>
+  );
 }
